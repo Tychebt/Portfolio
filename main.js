6cd9c5d850629c51f8455982bc8cded520094147
Tychebt <masse.ju35@gmail.com>
2021-06-07 09:31:35 +0200

    test slider

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-/*let currentSlide = $( "div.slider figure" )
-*/
+// /*let currentSlide = $( "div.slider figure" )
+// */
+// let sizeSlider = $( "div.slider figure img" ).length
+
+// let count = 0;
+// // for(let i = 0; i<document.querySelectorAll( "div.slider figure img" ).length;i++){
+   
+// let sliderInterval = window.setInterval(function(){ 
+//     if(count>sizeSlider*-500+500){
+//         document.querySelector("div.slider figure").style.left = `${count}px`
+//         count+= -500;
+        
+//     }else{
+//         count = 0
+//         document.querySelector("div.slider figure").style.left = `${count}px`
+//     }
+//   }, 1000);
+
+
+
 let sizeSlider = $( "div.slider figure img" ).length
 
 let count = 0;
-// for(let i = 0; i<document.querySelectorAll( "div.slider figure img" ).length;i++){
-    if(count < sizeSlider*500+500){
+   
 let sliderInterval = window.setInterval(function(){ 
-    if(count>sizeSlider*-500+500){
+    for(count = 0; i<sizeSlider*-500+500; i++){
+        document.querySelector("div.slider figure").style.left = `${count}px`
         count+= -500;
-        document.querySelector("div.slider figure").style.left = `${count}px`
-    }else{
-        count = 0
-        document.querySelector("div.slider figure").style.left = `${count}px`
+        console.log(count)
     }
+
   }, 1000);
-}
+
+
+/* Tant que count != sizeSliders
+left = 500px */
