6e60983f13312af028110d4c0ebf2a2f69fcb836
Tychebt <masse.ju35@gmail.com>
2021-06-08 09:25:31 +0200

    test slider

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-// /*let currentSlide = $( "div.slider figure" )
-// */
-// let sizeSlider = $( "div.slider figure img" ).length
-
-// let count = 0;
-// // for(let i = 0; i<document.querySelectorAll( "div.slider figure img" ).length;i++){
-   
-// let sliderInterval = window.setInterval(function(){ 
-//     if(count>sizeSlider*-500+500){
-//         document.querySelector("div.slider figure").style.left = `${count}px`
-//         count+= -500;
-        
-//     }else{
-//         count = 0
-//         document.querySelector("div.slider figure").style.left = `${count}px`
-//     }
-//   }, 1000);
-
-
-
+/*let currentSlide = $( "div.slider figure" )
+*/
 let sizeSlider = $( "div.slider figure img" ).length
 
 let count = 0;
+// for(let i = 0; i<document.querySelectorAll( "div.slider figure img" ).length;i++){
    
 let sliderInterval = window.setInterval(function(){ 
-    for(count = 0; i<sizeSlider*-500+500; i++){
+    if(count>sizeSlider*-500+500){
         document.querySelector("div.slider figure").style.left = `${count}px`
         count+= -500;
-        console.log(count)
+        
+    }else{
+        count = 0
+        document.querySelector("div.slider figure").style.left = `${count}px`
     }
-
   }, 1000);
+
+
+
+// let sizeSlider = $( "div.slider figure img" ).length
+
+// let count = 0;
+   
+// let sliderInterval = window.setInterval(function(){ 
+//     for(count = 0; i<sizeSlider*-500+500; i++){
+//         document.querySelector("div.slider figure").style.left = `${count}px`
+//         count+= -500;
+//         console.log(count)
+//     }
+
+//   }, 1000);
 
 
 /* Tant que count != sizeSliders
