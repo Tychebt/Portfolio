fbb64220bc65e9a032cabc49c35914be56aa0645
Tychebt <masse.ju35@gmail.com>
2021-06-24 11:38:48 +0200

    commentaires

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-/*let currentSlide = $( "div.slider figure" )
-*/
+// Slider compétences 
 let sizeSlider = $( "div.slider figure img" ).length
 
 let count = 0;
-//for(let i = 0; i<document.querySelectorAll( "div.slider figure img" ).length;i++){
    
 let sliderInterval = window.setInterval(function(){ 
     if(count>sizeSlider*-500){
@@ -17,29 +15,9 @@ let sliderInterval = window.setInterval(function(){
   }, 1000);
 
 
-
-// let sizeSlider = $( "div.slider figure img" ).length
-
-// let count = 0;
-   
-// let sliderInterval = window.setInterval(function(){ 
-//     for(count = 0; i<sizeSlider*-500+500; i++){
-//         document.querySelector("div.slider figure").style.left = `${count}px`
-//         count+= -500;
-//         console.log(count)
-//     }
-
-//   }, 1000);
-
-
-/* Tant que count != sizeSliders
-left = 500px */
-
-
-//Get the button:
+// Back to top 
 mybutton = document.getElementById("backToTopBTN");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -50,7 +28,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function backToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
