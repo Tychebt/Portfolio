fdbae1182267b88f9236594950fca098e40e57af
Tychebt <masse.ju35@gmail.com>
2021-06-26 16:43:55 +0200

    ecriture commentaire

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
- <?php
+ <?php //Connexion à la BDD
 $dsn = 'mysql:host=localhost;dbname=Portfolio;charset=utf8';
 try {
  
@@ -9,10 +9,34 @@ try {
      
      exit('Erreur de connexion à la base de données');
     }
+?>
 
-    $query = $pdo->query("SELECT * FROM `commentaires`");
+    <?php //Ecriture des commentaires
+    $req = $bdd->prepare('INSERT INTO commentaires(prenom, nom, mail, message) VALUES(:prenom, :nom, :mail, :message)');
+    $req->execute(array(
+        'nom' => $nom,
+        'prenom' => $possesseur,
+        'mail' => $console,
+        'message' => $message
+        ));
+    
+    echo 'Le comentaire a bien été ajouté !';
+    ?>
 
-    $resultat = $query->fetchAll();
-    echo json_encode($resultat);
+<?php //Lecture des commentaires
+    $query = $pdo->query("SELECT * FROM `commentaires` LIMIT 0, 5"); //Query SQL
+
+  while($query = $query->fetch())
+  {
+        ?>
+        <div class="wrapper-commentaire">
+            <p>Prénom : <?php echo $query['prenom'] ?></p>
+            <p>Nom : <?php echo $query['nom'] ?></p>
+            <p>E-mail : <?php echo $query['mail'] ?></p>
+            <p>Commentaire : <?php echo $query['message'] ?></p>
+        </div>
+            <?php
+  }
+
 ?> 
 
